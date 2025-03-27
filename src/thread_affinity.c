da5b5a85ce049fadc1e46e11662b0ce6564b86c8
0xrobomar <roberto.marino@unime.it>
2025-03-27 15:38:02 +0100

    12

--- src/thread_affinity.c
+++ src/thread_affinity.c
@@ -1,42 +1,58 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <pthread.h>
 #include <sched.h>
-
-#define NUM_THREADS 2
+#include <unistd.h>
 
 void* thread_function(void* arg) {
     int thread_num = *(int*)arg;
     cpu_set_t cpuset;
     
-    // Imposta l'affinity del thread
-    CPU_ZERO(&cpuset);
-    CPU_SET(thread_num, &cpuset);  // Lega il thread al core corrispondente al suo numero
+    // Configura l'affinity del thread
+    CPU_ZERO(&cpuset);               // Inizializza un set vuoto
+    CPU_SET(thread_num, &cpuset);    // Aggiunge il core specifico al set
     
-    // Simula un carico di lavoro
-    for (volatile int i = 0; i < 100000000; i++);  // Loop vuoto per consumare CPU
+    // Applica l'affinity al thread corrente
+    if (pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset) != 0) {
+        perror("Impossibile impostare l'affinità");
+    }
     
-    printf("Thread %d completato\n", thread_num);
+    // Verifica su quale core sta effettivamente girando il thread in question
+    int actual_core = sched_getcpu();
+    printf("Thread %d in esecuzione sul core %d\n", thread_num, actual_core);
+    
+    // Simula un workload
+    for (int i = 0; i < 10; i++) {
+        printf("Thread %d: lavoro %d/3\n", thread_num, i+1);
+        sleep(1);
+    }
+    
     return NULL;
 }
 
 int main() {
-    pthread_t threads[NUM_THREADS];
-    int thread_nums[NUM_THREADS];
+    pthread_t thread1, thread2;
+    int core1 = 0;  // Fissa il primo thread sul core 0
+    int core2 = 1;  // Fissa il secondo thread sul core 1
     
-    printf("Creazione di %d thread con affinity specifica\n", NUM_THREADS);
+    printf("Creazione thread sul core %d e %d\n", core1, core2);
     
-    for (int i = 0; i < NUM_THREADS; i++) {
-        thread_nums[i] = i;
-        if (pthread_create(&threads[i], NULL, thread_function, &thread_nums[i]) != 0) {
-            perror("pthread_create");
-            return 1;
-        }
+    // Crea il primo thread
+    if (pthread_create(&thread1, NULL, thread_function, &core1) != 0) {
+        perror("Errore creazione thread 1");
+        return 1;
     }
     
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
+    // Crea il secondo thread
+    if (pthread_create(&thread2, NULL, thread_function, &core2) != 0) {
+        perror("Errore creazione thread 2");
+        return 1;
     }
     
-    printf("Tutti i thread hanno completato l'esecuzione\n");
+    // Attesa terminazione thread
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    
+    printf("Tutti i thread hanno completato\n");
     return 0;
 }
